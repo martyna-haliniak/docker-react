576a3b540fad6e5167a0d5a983a70dca8d9af813
martyna <martynahaliniak@gmail.com>
2026-04-28 15:27:45 +0100

    updated App.test.js to match App.js changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          changed on feature branch 
+          changed on feature branch
         </a>
       </header>
     </div>
